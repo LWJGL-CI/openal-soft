3dceb2b8e3e9bf529d7f4291625fc7222ba8da16
Desu <desu@pokemmo.eu>
2019-05-01 21:16:17 +0000

    CI configuration for Linux arm64

--- common/wrap_memcpy.c
+++ common/wrap_memcpy.c
@@ -2,7 +2,13 @@
 
 void *old_memcpy(void *, const void *, size_t);
 
-__asm__(".symver old_memcpy,memcpy@GLIBC_2.2.5");
+#if defined(__aarch64__)
+  __asm__(".symver old_memcpy,memcpy@GLIBC_2.17");
+#elif defined(__arm__)
+  __asm__(".symver old_memcpy,memcpy@GLIBC_2.4");
+#else
+  __asm__(".symver old_memcpy,memcpy@GLIBC_2.2.5");
+#endif
 
 void *__wrap_memcpy(void *dest, const void *src, size_t n) {
 	return old_memcpy(dest, src, n);
